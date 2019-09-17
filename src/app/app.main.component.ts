880632cc088fd2af666b85c23003931d432938e6
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2019-09-17 07:03:01 +0000

    new default

--- src/app/app.main.component.ts
+++ src/app/app.main.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
     templateUrl: './app.main.component.html',
 })
 export class AppMainComponent {
-    menuMode = 'static';
+    menuMode = 'slim';
 
     overlayMenuActive: boolean;
 
