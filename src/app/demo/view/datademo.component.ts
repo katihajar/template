839c10d8d1a9a65b7eb6a87f4e579dadb94ff552
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2020-01-29 11:04:51 +0000

    carousel added

--- src/app/demo/view/datademo.component.ts
+++ src/app/demo/view/datademo.component.ts
@@ -71,6 +71,36 @@ import interactionPlugin from '@fullcalendar/interaction';
             text-align: left;
         }
 
+		.ui-carousel .ui-carousel-content .ui-carousel-item .car-details > .p-grid {
+			border: 1px solid #b3c2ca;
+			border-radius: 3px;
+			margin: 0.3em;
+			text-align: center;
+			padding: 2em 0 2.25em 0;
+		}
+		.ui-carousel .ui-carousel-content .ui-carousel-item .car-data .car-title {
+			font-weight: 700;
+			font-size: 20px;
+			margin-top: 24px;
+		}
+		.ui-carousel .ui-carousel-content .ui-carousel-item .car-data .car-subtitle {
+			margin: 0.25em 0 2em 0;
+		}
+		.ui-carousel .ui-carousel-content .ui-carousel-item .car-data button {
+			margin-left: 0.5em;
+		}
+		.ui-carousel .ui-carousel-content .ui-carousel-item .car-data button:first-child {
+			margin-left: 0;
+		}
+		.ui-carousel.custom-carousel .ui-carousel-dot-icon {
+			width: 16px !important;
+			height: 16px !important;
+			border-radius: 50%;
+		}
+		.ui-carousel.ui-carousel-horizontal .ui-carousel-content .ui-carousel-item.ui-carousel-item-start .car-details > .p-grid {
+			margin-left: 0.6em;
+		}
+
         @media (max-width: 40em) {
             .car-item {
                 text-align: center;
@@ -125,6 +155,8 @@ export class DataDemoComponent implements OnInit {
     orderListCars: Car[];
 
     carouselCars: Car[];
+
+    responsiveOptions;
 
     files1: TreeNode[];
 
@@ -195,6 +227,23 @@ export class DataDemoComponent implements OnInit {
             { vin: 'he6sb5v', year: 2015, brand: 'Ford', color: 'Black' }
         ];
 
+        this.responsiveOptions = [
+            {
+                breakpoint: '1024px',
+                numVisible: 3,
+                numScroll: 3
+            },
+            {
+                breakpoint: '768px',
+                numVisible: 2,
+                numScroll: 2
+            },
+            {
+                breakpoint: '560px',
+                numVisible: 1,
+                numScroll: 1
+            }
+        ];
 
         this.fullcalendarOptions = {
             plugins: [ dayGridPlugin, timeGridPlugin, interactionPlugin ],
